26e1d9c029f1f07104eeb3ae1862dc32151aad23
Your Name <you@example.com>
2026-09-21 16:15:27 +0500

    fix(build): resolve KernelSU-Next 4.14 timekeeping error and disable driver signature verification

--- kernel/module.c
+++ kernel/module.c
@@ -312,10 +312,8 @@ static void module_assert_mutex_or_preempt(void)
 #endif
 }
 
-static bool sig_enforce = IS_ENABLED(CONFIG_MODULE_SIG_FORCE);
-#ifndef CONFIG_MODULE_SIG_FORCE
-module_param(sig_enforce, bool_enable_only, 0644);
-#endif /* !CONFIG_MODULE_SIG_FORCE */
+static bool sig_enforce = false;
+module_param(sig_enforce, bool, 0644);
 
 /* Block module loading/unloading? */
 int modules_disabled = 0;
